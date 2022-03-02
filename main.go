948711c8fef70800e4efacbdb41290c2755156b5
iwanssasas <puji.setiawan@students.amikom.ac.id>
2022-03-02 13:12:18 +0700

    nghapus migration test

--- main.go
+++ main.go
@@ -9,8 +9,3 @@ func main() {
 	config.Init()
 	server.NewServer()
 }
-
-//created_at
-// created_by
-// updated_at
-// updated_by
--- migrations/20220302120304-create_table_test.sql
+++
@@ -1,7 +0,0 @@
--- +migrate Up
-CREATE TABLE test (
-    id int(2) AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100)
-);
--- +migrate Down
-DROP TABLE test;
